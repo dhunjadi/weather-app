38c878fbc0f1f641dd4a46d41bd8351569732e2b
dhunjadi <74426940+dhunjadi@users.noreply.github.com>
2021-06-10 19:22:24 +0200

    Add files via upload

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -3,20 +3,29 @@ import Future from './Future'
 
 export default function Forecast(props) {
 
+
+    let array = props.forecast.list
+
+    function show(){
+        console.log(array.slice(1, 3))
+    }
+
     return (
         (typeof props.forecast.city != 'undefined') ? (
             <div className="forecast-container">
                 <div className="temp">
                     {Math.round(props.forecast.list[0].main.temp)}°C<hr />
                 </div>
-                <div className="forecast-today">
+                <div onClick={show} className="forecast-today">
                     {props.forecast.list[0].weather[0].main}
                 </div>
                 <Future
                     forecast={props.forecast}
+                    
                 />
             </div>
 
         ) : ('')
     )
 }
+
